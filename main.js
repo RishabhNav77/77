5257a74eb12e23331f823adcdb29df0819e35d25
RishabhNav77 <73402371+RishabhNav77@users.noreply.github.com>
2020-12-05 20:49:32 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,30 @@
+console.log("hi i am console screen");
+
+var first="Ranveer";
+console.log(first);
+
+var last="kapoor";
+console.log(last);
+
+var full=first.concat(last);
+console.log(full);
+
+var name="i am Ranveer kapoor";
+console.log(name);
+
+var replace=name.replace("kapoor","Rishi kapoor");
+console.log(replace);
+
+var n1=12;
+console.log(n1);
+
+var n2=14;
+console.log(n2);
+
+console.log(n1+n2);
+console.log(n1*n2);
+console.log(n2/n1);
+console.log(n2-n1);
+
+
+
